d8f4cd71a348329e162b5cffbbfa67189c5c89dc
fuziwang <2622860598@qq.com>
2018-10-23 17:35:40 +0800

    cd mkdir

---
+++ Linux advanced/ch02/src/cd.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+int main (int argc, char *argv[])
+{
+    if (argc<2) {
+        if(chdir(getenv("HOME"))<0){
+            perror("chdir");
+            return -1;
+        }
+    }
+    else if (chdir(argv[1])<0) {
+        perror("chdir");
+        return -1;
+    }
+    char buf[512] = {'\0'};
+
+    return 0;
+}
